e1128e8b35cc0a11f76e83dc6752aa42f7ea0db2
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-15 13:07:31 +0100

    Spring playground

---
+++ src/components/ModifierPlayground/Spring.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  WithSpringConfig,
+  cancelAnimation,
+} from "react-native-reanimated";
+
+const initialOffset = -200;
+
+interface Props {
+  options: WithSpringConfig;
+}
+
+export default function App({ options }: Props) {
+  const offset = useSharedValue(initialOffset);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value }],
+    };
+  });
+
+  useEffect(() => {
+    cancelAnimation(offset);
+  }, [options]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      offset.value = withSpring(-initialOffset, options, (isFinished) => {
+        setTimeout(() => {
+          if (isFinished) {
+            offset.value = initialOffset;
+          }
+        }, 1000);
+      });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [options]);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <View style={[styles.box, styles.ghost]} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  box: {
+    height: 100,
+    width: 100,
+    backgroundColor: "#001A72",
+    borderRadius: 20,
+    marginVertical: 64,
+  },
+  ghost: {
+    opacity: 0.2,
+    position: "absolute",
+    transform: [{ translateX: initialOffset }],
+  },
+});
---
+++ src/components/ModifierPlayground/index.tsx
@@ -0,0 +1,168 @@
+import React, { Dispatch } from "react";
+import styles from "./styles.module.css";
+
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import Spring from "./Spring";
+
+export default function ModifierPlayground(props: any) {
+  const [key, setKey] = React.useState(0);
+  const [copied, setCopied] = React.useState(false);
+
+  const [damping, setDamping] = React.useState(10);
+  const [mass, setMass] = React.useState(1);
+  const [stiffness, setStiffness] = React.useState(100);
+  const [overshootClamping, setOvershootClamping] = React.useState(false);
+  const [restDisplacementThreshold, setRestDisplacementThreshold] =
+    React.useState(21.37);
+  const [restSpeedThreshold, setRestSpeedThreshold] = React.useState(2);
+
+  const resetExample = () => {
+    setKey(key + 1);
+    setCopied(false);
+  };
+
+  const code = `
+    withSpring(sv.value, {
+      damping: ${damping},
+      mass: ${mass},
+      stiffness: ${stiffness},
+      overshootClamping: ${overshootClamping},
+      restDisplacementThreshold: ${restDisplacementThreshold},
+      restSpeedThreshold: ${restSpeedThreshold},
+    })
+  `;
+
+  return (
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => (
+        <div className={styles.container}>
+          <div className={styles.buttonContainer}>
+            <button onClick={resetExample}>Reset</button>
+          </div>
+          <React.Fragment key={key}>
+            <Spring
+              options={{
+                damping,
+                mass,
+                stiffness,
+                overshootClamping,
+                restDisplacementThreshold,
+                restSpeedThreshold,
+              }}
+            />
+          </React.Fragment>
+          <div className={styles.wrapper}>
+            <div className={styles.sliders}>
+              <Range
+                label="Damping"
+                min={1}
+                max={100}
+                value={damping}
+                onChange={setDamping}
+              />
+              <Range
+                label="Mass"
+                min={1}
+                max={10}
+                value={mass}
+                onChange={setMass}
+              />
+
+              <Range
+                label="Stiffness"
+                min={1}
+                max={1000}
+                value={stiffness}
+                onChange={setStiffness}
+              />
+              <Checkbox
+                label="Overshoot clamping"
+                value={overshootClamping}
+                onChange={setOvershootClamping}
+              />
+
+              <Range
+                label="Rest displacement threshold"
+                min={0.01}
+                max={150}
+                step={0.01}
+                value={restDisplacementThreshold}
+                onChange={setRestDisplacementThreshold}
+              />
+
+              <Range
+                label="Rest speed threshold"
+                min={0.01}
+                max={150}
+                step={0.01}
+                value={restSpeedThreshold}
+                onChange={setRestSpeedThreshold}
+              />
+            </div>
+            <div className={styles.codeWrapper}>
+              <SyntaxHighlighter
+                className={styles.code}
+                language="javascript"
+                style={docco}
+              >
+                {code}
+              </SyntaxHighlighter>
+            </div>
+          </div>
+        </div>
+      )}
+    </BrowserOnly>
+  );
+}
+
+interface RangeProps {
+  min: number;
+  max: number;
+  step?: number;
+  value: number;
+  onChange: Dispatch<number>;
+  label: string;
+}
+
+function Range({ min, max, value, onChange, label, step = 1 }: RangeProps) {
+  return (
+    <>
+      <div>
+        {label}: {value}
+      </div>
+      <div className={styles.row}>
+        <div className={styles.label}>{min}</div>
+        <input
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={(e) => onChange(parseFloat(e.target.value))}
+        />
+        <div className={styles.label}>{max}</div>
+      </div>
+    </>
+  );
+}
+
+interface CheckboxProps {
+  value: boolean;
+  onChange: Dispatch<boolean>;
+  label: string;
+}
+
+function Checkbox({ value, onChange, label }: CheckboxProps) {
+  return (
+    <div className={styles.row}>
+      <div>{label}:</div>
+      <input
+        type="checkbox"
+        checked={value}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+    </div>
+  );
+}
