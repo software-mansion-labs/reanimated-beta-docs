47d30d00e142fe573b6696edfb398d466bf8b74d
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-16 17:01:55 +0100

    Make modifier playground composable

--- src/components/ModifierPlayground/index.tsx
+++ src/components/ModifierPlayground/index.tsx
@@ -4,35 +4,18 @@ import styles from "./styles.module.css";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import Spring from "./Spring";
+import useSpringPlayground from "./useSpringPlayground";
 
 export default function ModifierPlayground(props: any) {
   const [key, setKey] = React.useState(0);
   const [copied, setCopied] = React.useState(false);
 
-  const [damping, setDamping] = React.useState(10);
-  const [mass, setMass] = React.useState(1);
-  const [stiffness, setStiffness] = React.useState(100);
-  const [overshootClamping, setOvershootClamping] = React.useState(false);
-  const [restDisplacementThreshold, setRestDisplacementThreshold] =
-    React.useState(0.01);
-  const [restSpeedThreshold, setRestSpeedThreshold] = React.useState(2);
+  const { example, code, controls } = useSpringPlayground();
 
   const resetExample = () => {
     setKey(key + 1);
     setCopied(false);
   };
-
-  const code = `
-    withSpring(sv.value, {
-      damping: ${damping},
-      mass: ${mass},
-      stiffness: ${stiffness},
-      overshootClamping: ${overshootClamping},
-      restDisplacementThreshold: ${restDisplacementThreshold},
-      restSpeedThreshold: ${restSpeedThreshold},
-    })
-  `;
 
   return (
     <BrowserOnly fallback={<div>Loading...</div>}>
@@ -41,66 +24,9 @@ export default function ModifierPlayground(props: any) {
           <div className={styles.buttonContainer}>
             <button onClick={resetExample}>Reset</button>
           </div>
-          <React.Fragment key={key}>
-            <Spring
-              options={{
-                damping,
-                mass,
-                stiffness,
-                overshootClamping,
-                restDisplacementThreshold,
-                restSpeedThreshold,
-              }}
-            />
-          </React.Fragment>
+          <React.Fragment key={key}>{example}</React.Fragment>
           <div className={styles.wrapper}>
-            <div className={styles.sliders}>
-              <Range
-                label="Damping"
-                min={1}
-                max={100}
-                value={damping}
-                onChange={setDamping}
-              />
-              <Range
-                label="Mass"
-                min={1}
-                max={10}
-                value={mass}
-                onChange={setMass}
-              />
-
-              <Range
-                label="Stiffness"
-                min={1}
-                max={1000}
-                value={stiffness}
-                onChange={setStiffness}
-              />
-              <Checkbox
-                label="Overshoot clamping"
-                value={overshootClamping}
-                onChange={setOvershootClamping}
-              />
-
-              <Range
-                label="Rest displacement threshold"
-                min={0.01}
-                max={150}
-                step={0.01}
-                value={restDisplacementThreshold}
-                onChange={setRestDisplacementThreshold}
-              />
-
-              <Range
-                label="Rest speed threshold"
-                min={0.01}
-                max={150}
-                step={0.01}
-                value={restSpeedThreshold}
-                onChange={setRestSpeedThreshold}
-              />
-            </div>
+            <div className={styles.controls}>{controls}</div>
             <div className={styles.codeWrapper}>
               <SyntaxHighlighter
                 className={styles.code}
@@ -126,24 +52,35 @@ interface RangeProps {
   label: string;
 }
 
-function Range({ min, max, value, onChange, label, step = 1 }: RangeProps) {
+export function Range({
+  min,
+  max,
+  value,
+  onChange,
+  label,
+  step = 1,
+}: RangeProps) {
   return (
     <>
-      <div>
-        {label}: {value}
-      </div>
       <div className={styles.row}>
-        <div className={styles.label}>{min}</div>
+        <div>{label}</div>
         <input
-          type="range"
+          type="number"
           min={min}
           max={max}
           step={step}
           value={value}
           onChange={(e) => onChange(parseFloat(e.target.value))}
         />
-        <div className={styles.label}>{max}</div>
       </div>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onChange={(e) => onChange(parseFloat(e.target.value))}
+      />
     </>
   );
 }
@@ -154,10 +91,10 @@ interface CheckboxProps {
   label: string;
 }
 
-function Checkbox({ value, onChange, label }: CheckboxProps) {
+export function Checkbox({ value, onChange, label }: CheckboxProps) {
   return (
     <div className={styles.row}>
-      <div>{label}:</div>
+      <div>{label}</div>
       <input
         type="checkbox"
         checked={value}
---
+++ src/components/ModifierPlayground/useSpringPlayground/index.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Example from "./Example";
+
+import { Range, Checkbox } from "..";
+
+export default function useSpringExample() {
+  const [damping, setDamping] = useState(10);
+  const [mass, setMass] = useState(1);
+  const [stiffness, setStiffness] = useState(100);
+  const [velocity, setVelocity] = useState(0);
+  const [overshootClamping, setOvershootClamping] = useState(false);
+  const [restDisplacementThreshold, setRestDisplacementThreshold] =
+    useState(0.01);
+  const [restSpeedThreshold, setRestSpeedThreshold] = useState(2);
+
+  const code = `
+    withSpring(sv.value, {
+      damping: ${damping},
+      mass: ${mass},
+      stiffness: ${stiffness},
+      velocity: ${velocity},
+      overshootClamping: ${overshootClamping},
+      restDisplacementThreshold: ${restDisplacementThreshold},
+      restSpeedThreshold: ${restSpeedThreshold},
+    })
+  `;
+
+  const controls = (
+    <>
+      <Range
+        label="Damping"
+        min={1}
+        max={100}
+        value={damping}
+        onChange={setDamping}
+      />
+      <Range
+        label="Mass"
+        min={1}
+        max={20}
+        step={0.1}
+        value={mass}
+        onChange={setMass}
+      />
+      <Range
+        label="Stiffness"
+        min={1}
+        max={500}
+        value={stiffness}
+        onChange={setStiffness}
+      />
+      <Range
+        label="Velocity"
+        min={-50}
+        max={50}
+        value={velocity}
+        onChange={setVelocity}
+      />
+      <Checkbox
+        label="Clamp"
+        value={overshootClamping}
+        onChange={setOvershootClamping}
+      />
+      <Range
+        label="Displacement threshold"
+        min={0.01}
+        max={150}
+        step={0.01}
+        value={restDisplacementThreshold}
+        onChange={setRestDisplacementThreshold}
+      />
+      <Range
+        label="Speed threshold"
+        min={0.01}
+        max={150}
+        step={0.01}
+        value={restSpeedThreshold}
+        onChange={setRestSpeedThreshold}
+      />
+    </>
+  );
+
+  const example = (
+    <Example
+      options={{
+        damping,
+        mass,
+        stiffness,
+        velocity,
+        overshootClamping,
+        restDisplacementThreshold,
+        restSpeedThreshold,
+      }}
+    />
+  );
+
+  return {
+    example,
+    controls,
+    code,
+  };
+}
