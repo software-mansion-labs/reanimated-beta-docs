43b445f4a08c8fc326eb1c791f0dd88deb4bd564
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-21 16:40:34 +0100

    withRepeat

--- src/examples/RepeatBasic/index.js
+++ src/examples/RepeatBasic/index.js
@@ -9,13 +9,13 @@ import { StyleSheet, View } from "react-native";
 import React, { useEffect } from "react";
 
 const duration = 2000;
-const easing = Easing.bezier(0.25, -0.8, 0.25, 1);
+const easing = Easing.bezier(0.25, -0.5, 0.25, 1);
 
 export default function App() {
   const sv = useSharedValue(0);
 
   useEffect(() => {
-    sv.value = withRepeat(withTiming(1, { duration, easing }), -1);
+    sv.value = withRepeat(withTiming(1, { duration, easing }), -1, true);
   }, []);
 
   const animatedStyle = useAnimatedStyle(() => ({
