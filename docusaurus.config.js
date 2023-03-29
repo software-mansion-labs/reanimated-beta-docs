efebfbab80d8543267ffb2ccb2ade3fc68e480f1
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-03-29 14:26:55 +0200

    update site title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,7 @@ const webpack = require("webpack");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "React Native Reanimated",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -56,7 +55,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "React Native Reanimated",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -76,27 +75,8 @@ const config = {
         ],
       },
       footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        style: "light",
+        links: [],
       },
       prism: {
         theme: lightCodeTheme,
