60844b2b32fbbf941730ef14ab35087e951d7f15
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-04-13 11:39:44 +0200

    fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: "https://beta-docs.swmansion.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-native-reanimated",
+  baseUrl: "/react-native-reanimated/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
