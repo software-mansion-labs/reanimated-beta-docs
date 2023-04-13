8be4f8066fb10bee4424b43e8e6e8cf447a2aeb0
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-04-13 12:37:06 +0200

    build docs in react-native-reanimated folder
    maybe I am a psychopath in the end

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,9 +13,9 @@ const config = {
 
   // Set the production url of your site here
   url: "https://beta-docs.swmansion.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-native-reanimated/",
+
+  // Change this to /react-native-reanimated/ when deploying to GitHub pages and update `serve` script in package.json to `docusaurus serve`
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
