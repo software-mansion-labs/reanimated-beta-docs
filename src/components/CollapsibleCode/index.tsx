d0b390850bb18c06d82bcb9f247319818239c9e7
tboba <tymoteusz.boba@gmail.com>
2023-04-18 15:33:59 +0200

    Add styling for collapsible code

--- src/components/CollapsibleCode/index.tsx
+++ src/components/CollapsibleCode/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import CodeBlock from "@theme/CodeBlock";
+import styles from "./styles.module.css";
+
+import Arrow from "@site/static/img/Arrow.svg";
+import ArrowDark from "@site/static/img/Arrow-dark.svg";
+import { useColorMode } from "@docusaurus/theme-common";
 
 interface Props {
   src: string;
@@ -8,6 +13,7 @@ interface Props {
 
 export default function CollapsibleCode({ src, showLines }: Props) {
   const [expand, setExpand] = React.useState(false);
+  const { colorMode } = useColorMode();
 
   if (!showLines) {
     return <CodeBlock language="jsx">{src}</CodeBlock>;
@@ -19,10 +25,22 @@ export default function CollapsibleCode({ src, showLines }: Props) {
   const linesToShow = codeLines.slice(start, end + 1).join("\n");
 
   return (
-    <div>
-      <button onClick={() => setExpand(!expand)}>
-        {expand ? "Collapse" : "Expand"}
-      </button>
+    <div className={styles.container}>
+      <div
+        className={styles.buttonContainer}
+        data-isopen={expand}
+        onClick={() => setExpand(!expand)}
+      >
+        {colorMode === "light" ? (
+          <Arrow className={styles.arrow} />
+        ) : (
+          <ArrowDark className={styles.arrow} />
+        )}
+
+        <button>
+          {expand ? "Collapse the full code" : "Expand the full code"}
+        </button>
+      </div>
       <CodeBlock language="jsx">{expand ? src : linesToShow}</CodeBlock>
     </div>
   );
