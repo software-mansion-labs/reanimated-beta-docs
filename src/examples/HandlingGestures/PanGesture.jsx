cc4f9ab8d5e6d4c4a3e9208804753dbb192bbea6
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-04-21 10:58:45 +0200

    draft handling gestures

---
+++ src/examples/HandlingGestures/PanGesture.jsx
@@ -0,0 +1,57 @@
+import "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+import React from "react";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+
+export default function App() {
+  const offset = useSharedValue(0);
+
+  const pan = Gesture.Pan()
+    .minDistance(0)
+    .onChange((event) => {
+      offset.value = event.translationX;
+    })
+    .onFinalize(() => {
+      offset.value = withSpring(0);
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[styles.box, animatedStyles]} />
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+  box: {
+    height: 120,
+    width: 120,
+    backgroundColor: "#b58df1",
+    borderRadius: 20,
+    cursor: "grab",
+  },
+});
--- src/examples/HandlingGestures/TapGesture.jsx
+++ src/examples/HandlingGestures/TapGesture.jsx
@@ -1,8 +1,9 @@
+import "react-native-gesture-handler";
 import { StyleSheet, View } from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
-  withSpring,
+  withTiming,
 } from "react-native-reanimated";
 
 import React from "react";
@@ -12,27 +13,27 @@ import {
   GestureHandlerRootView,
 } from "react-native-gesture-handler";
 
-export default function BouncingBoxExample() {
-  const offset = useSharedValue(0);
+export default function App() {
+  const pressed = useSharedValue(false);
 
-  const pan = Gesture.Pan()
-    .minDistance(0)
-    .onChange((event) => {
-      offset.value = event.translationX;
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      pressed.value = true;
     })
     .onFinalize(() => {
-      offset.value = withSpring(0);
+      pressed.value = false;
     });
 
   const animatedStyles = useAnimatedStyle(() => ({
-    transform: [{ translateX: offset.value }],
+    backgroundColor: withTiming(pressed.value ? "#FFE04B" : "#b58df1"),
+    transform: [{ scale: withTiming(pressed.value ? 1.2 : 1) }],
   }));
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <GestureDetector gesture={pan}>
-          <Animated.View style={[styles.box, animatedStyles]}></Animated.View>
+        <GestureDetector gesture={tap}>
+          <Animated.View style={[styles.circle, animatedStyles]} />
         </GestureDetector>
       </View>
     </GestureHandlerRootView>
@@ -46,11 +47,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: "100%",
   },
-  box: {
+  circle: {
     height: 120,
     width: 120,
-    backgroundColor: "#b58df1",
-    borderRadius: 20,
-    cursor: "grab",
+    borderRadius: 500,
   },
 });
