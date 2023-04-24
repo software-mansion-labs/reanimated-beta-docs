ed71a399d5c3359b18af416a379c229ff876ef41
tboba <tymoteusz.boba@gmail.com>
2023-04-24 17:17:08 +0200

    Add fixes, regarding server-side rendering

--- src/components/Hero/StartScreen/index.tsx
+++ src/components/Hero/StartScreen/index.tsx
@@ -8,15 +8,34 @@ import Sun from "@site/src/components/Hero/Sun";
 import Stars from "@site/src/components/Hero/Stars";
 import Horse from "@site/src/components/Hero/Horse";
 
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
+const Background = () => {
+  return (
+    <div className={styles.heroBackground}>
+      <Clouds />
+      <Stars />
+
+      {
+        /* Swirl uses viewport behind the hood to calculate appropriate with.
+         * Thus, access to the viewport is required to render the Swirl component.
+         */
+        ExecutionEnvironment.canUseViewport && (
+          <>
+            <Sun />
+            <Swirl />
+          </>
+        )
+      }
+    </div>
+  );
+};
+
 const StartScreen = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroBackground}>
-        <Clouds />
-        <Stars />
-        <Sun />
-        <Swirl />
-      </div>
+      <Background />
+
       <div className={styles.foregroundLabel}>
         <div className={styles.heading}>
           <div className={styles.upperHeading}>
