4470be8bebc4e96229f5abea0ab1aed5e6e55b67
tboba <tymoteusz.boba@gmail.com>
2023-04-27 15:12:07 +0200

    Add linking to brand in footer

--- src/theme/Footer/Copyright/index.js
+++ src/theme/Footer/Copyright/index.js
@@ -8,6 +8,8 @@ import ThemedImage from "@theme/ThemedImage";
 import BrandLogo from "@site/static/img/brand-dark.svg";
 import BrandLogoDark from "@site/static/img/brand-dark.svg";
 
+const BRAND_LINK = "https://swmansion.com/";
+
 export default function FooterCopyright({ copyright }) {
   const location = useLocation();
   const isDocumentation = location.pathname.startsWith("/docs");
@@ -19,14 +21,20 @@ export default function FooterCopyright({ copyright }) {
 
   return (
     <div className={clsx("footer__copyright", styles.footer)}>
-      {!isDocumentation && (
-        <div className={styles.footer__logo}>
-          <ThemedImage sources={brandLogo} />
-        </div>
-      )}
+      {
+        <a href={BRAND_LINK} target="_blank">
+          <div className={styles.footer__logo}>
+            <ThemedImage sources={brandLogo} />
+          </div>
+        </a>
+      }
       <p className={clsx(!isDocumentation && styles.landing)}>
         <span className={styles.footer__brand}>
-          &copy; Software Mansion {new Date().getFullYear()}.
+          &copy;{" "}
+          <a href={BRAND_LINK} target="_blank">
+            Software Mansion
+          </a>
+          {" " + new Date().getFullYear()}.
         </span>
         {` ${copyright}`}
       </p>
