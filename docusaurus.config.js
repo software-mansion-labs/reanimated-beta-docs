46606cca166c00018f1d955f76b16fc2af4f9006
tboba <tymoteusz.boba@gmail.com>
2023-04-28 12:49:11 +0200

    Remove obsolete images, add new og image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/social-card.svg",
       navbar: {
         title: "React Native Reanimated",
         hideOnScroll: true,
