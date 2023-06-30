f2c19019bdf3865f7e5ce47569d2528f04066729
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2023-06-30 15:54:58 +0200

    Add entering/exiting animations (#30)

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -5,6 +5,9 @@ import InteractivePlayground from "@site/src/components/InteractivePlayground";
 import CollapsibleCode from "@site/src/components/CollapsibleCode";
 import Optional from "@site/src/components/Optional";
 import Indent from "@site/src/components/Indent";
+import Row from "@site/src/components/Row";
+import Grid from "@site/src/components/Grid";
+import ThemedVideo from "@site/src/components/ThemedVideo";
 
 export default {
   // Re-use the default mapping
@@ -16,4 +19,7 @@ export default {
   CollapsibleCode,
   Optional,
   Indent,
+  Row,
+  Grid,
+  ThemedVideo,
 };
