832723a8b04c1525291879c47265905d6b990346
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-08-07 10:08:17 +0200

    Fix sidebar labels

--- src/theme/DocSidebarItems/index.js
+++ src/theme/DocSidebarItems/index.js
@@ -4,8 +4,8 @@ import DocSidebarItem from "@theme/DocSidebarItem";
 import SidebarLabel from "@site/src/components/SidebarLabel";
 import styles from "./styles.module.css";
 
-const EXPERIMENTAL_APIs = ["Overview"]; // this will break if there's more than one "Overview" item
-const NEW_APIS = ["dispatchCommand"];
+const EXPERIMENTAL_APIs = ["shared-element-transitions/overview"];
+const NEW_APIS = ["advanced/dispatchCommand"];
 // TODO this item should probably not receive the "activePath" props
 // TODO this triggers whole sidebar re-renders on navigation
 function DocSidebarItems({ items, ...props }) {
@@ -14,11 +14,11 @@ function DocSidebarItems({ items, ...props }) {
       {items.map((item, index) => (
         <div className={styles.wrapper}>
           <DocSidebarItem key={index} item={item} index={index} {...props} />
-          {EXPERIMENTAL_APIs.includes(item.label) && (
-            <SidebarLabel key={`${index}-label`} type="experimental" />
+          {EXPERIMENTAL_APIs.includes(item.docId) && (
+            <SidebarLabel key={item.docId} type="experimental" />
           )}
-          {NEW_APIS.includes(item.label) && (
-            <SidebarLabel key={`${index}-label`} type="new" />
+          {NEW_APIS.includes(item.docId) && (
+            <SidebarLabel key={item.docId} type="new" />
           )}
         </div>
       ))}
