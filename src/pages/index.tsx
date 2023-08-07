6fded5dad824e9bdea9d80fa32ffbeb07e47a7e1
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-08-07 09:23:59 +0200

    Update og:description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import ReanimatedSponsors from "@site/src/components/ReanimatedSponsors";
 
 export default function Home(): JSX.Element {
   return (
-    <Layout description="React Native Reanimated is a powerful animation library that makes it easy to create smooth animations and interactions that run in the UI thread.">
+    <Layout description="A powerful animation library that makes it easy to create smooth animations and interactions that run in the UI thread.">
       <div className={styles.landingContainer}>
         <HomepageStartScreen />
         <ReanimatedFeatures />
